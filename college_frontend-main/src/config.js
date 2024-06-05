5f5d5f5356c9b0b0ea4043b3ea0dc07c49b61882
Ayush Meena <ayushmeena22@iitk.ac.in>
2024-06-05 23:23:30 +0530

    adding files

---
+++ college_frontend-main/src/config.js
@@ -0,0 +1,7 @@
+export const api = {  
+  tagsEndPoint: 'https://college-backend-tyqu.onrender.com/tags/',
+  usersEndPoint: 'https://college-backend-tyqu.onrender.com/users/',
+  postsEndPoint: 'https://college-backend-tyqu.onrender.com/posts/',
+  repliesEndPoint: 'https://college-backend-tyqu.onrender.com/reply/',
+  otpEndPoint: 'https://college-backend-tyqu.onrender.com/otp/',
+}
